2a38b84e08c5478783303dd6c869d6a562350984
ammar2303a <ammar2303a@aptechgdn.net>
2025-10-14 10:39:23 +0500

    pkg update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import { useEffect } from 'react'
+
 function App() {
   return(
     <BrowserRouter>
   <Nav/>
 <Routes>
+  
   <Route path='/' element={<Home/>}/>
 </Routes>
   <Footer/>
